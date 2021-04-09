be8e9a2c9a43e5626bd05c0af6889ea677f9e158
ssunnu <66551410+whai47@users.noreply.github.com>
2021-04-10 04:22:30 +0900

    #1 complete Clear

---
+++ Weather.js
@@ -0,0 +1,79 @@
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+import PropTypes from "prop-types";
+import { MaterialCommunityIcons } from "@expo/vector-icons";
+import { LinearGradient } from "expo-linear-gradient";
+
+const weatherOptions = {
+  Clear: {
+    iconName: "white-balance-sunny",
+    gradient: ["#2980b9", "#6dd5fa", "#ffffff"],
+    title: "오늘은 맑습니다",
+    subtitle: "외출하기에 좋습니다",
+  },
+};
+
+export default function Weather({ temp, condition }) {
+  return (
+    <LinearGradient
+      colors={weatherOptions[condition].gradient}
+      style={styles.container}
+    >
+      <View style={styles.halfContainer}>
+        <MaterialCommunityIcons
+          name={weatherOptions[condition].iconName}
+          size={96}
+          color="white"
+        />
+        <Text style={styles.temp}>{temp}℃</Text>
+      </View>
+      <View style={styles.halfContainer}>
+        <Text style={styles.title}>{weatherOptions[condition].title}</Text>
+        <Text style={styles.subtitle}>
+          {weatherOptions[condition].subtitle}
+        </Text>
+      </View>
+    </LinearGradient>
+  );
+}
+
+Weather.propTypes = {
+  temp: PropTypes.number.isRequired,
+  condition: PropTypes.oneOf([
+    "Thunderstorm",
+    "Drizzle",
+    "Rain",
+    "Snow",
+    "Atmoshere",
+    "Clear",
+    "Clouds",
+  ]).isRequired,
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  temp: {
+    fontSize: 42,
+    color: "white",
+  },
+  halfContainer: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  title: {
+    color: "white",
+    fontSize: 44,
+    fontWeight: "100",
+    marginBottom: 10,
+  },
+  subtitle: {
+    color: "white",
+    fontWeight: "800",
+    fontSize: 32,
+  },
+});
